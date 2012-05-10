14c8e87a7eb490429ce1732260e1e4fa669637dd
mamontov <Rgg60fbd>
2012-05-09 18:29:23 -0700

    Improve reader admin component

--- server/src/main/java/edu/exigen/client/gui/ReaderAdminComponent.java
+++ server/src/main/java/edu/exigen/client/gui/ReaderAdminComponent.java
@@ -27,6 +27,12 @@ public class ReaderAdminComponent {
     private static final String UPDATE_BUTTON_NAME = "Update";
     private static final String DELETE_BUTTON_NAME = "Delete";
 
+    private static final String FIRST_NAME = "First Name:";
+    private static final String LAST_NAME = "Last Name:";
+    private static final String ADDRESS = "Address:";
+    private static final String DATE_OF_BIRTH = "Date of birth:";
+    private static final String CURRENTLY_RESERVED_BOOKS = "Currently reserved books:";
+
     private ReaderProvider readerProvider;
     private ReaderSearchComponent searchComponent;
     private Reader tableReader;
@@ -86,24 +92,24 @@ public class ReaderAdminComponent {
         JPanel adminPanel = new JPanel();
         adminPanel.setLayout(new GridBagLayout());
         java.util.List<JComponent> adminComponents = new ArrayList<JComponent>();
-        JLabel firstNameLabel = new JLabel("First Name :");
+        JLabel firstNameLabel = new JLabel(FIRST_NAME);
         adminComponents.add(firstNameLabel);
         firstNameField = new JTextField();
         adminComponents.add(firstNameField);
-        JLabel lastNameLabel = new JLabel("Last Name :");
+        JLabel lastNameLabel = new JLabel(LAST_NAME);
         adminComponents.add(lastNameLabel);
         lastNameField = new JTextField();
         adminComponents.add(lastNameField);
-        JLabel addressLabel = new JLabel("Address:");
+        JLabel addressLabel = new JLabel(ADDRESS);
         adminComponents.add(addressLabel);
         addressField = new JTextField();
         adminComponents.add(addressField);
-        JLabel dateOfBirthLabel = new JLabel("Date of birth:");
+        JLabel dateOfBirthLabel = new JLabel(DATE_OF_BIRTH);
         adminComponents.add(dateOfBirthLabel);
         dateOfBirthField = new JTextField();
         dateOfBirthField.setText(LibraryConstraints.LIBRARY_DATE_PATTERN);
         adminComponents.add(dateOfBirthField);
-        JLabel reservedBooksLabel = new JLabel("Currently reserved books:");
+        JLabel reservedBooksLabel = new JLabel(CURRENTLY_RESERVED_BOOKS);
         adminComponents.add(reservedBooksLabel);
         reservedBooksList = new JList(readerBooksInfo);
         reservedBooksList.setEnabled(false);
@@ -118,7 +124,7 @@ public class ReaderAdminComponent {
         c.weightx = 1;
         c.gridx = 0;
         c.gridy = 0;
-        c.fill = GridBagConstraints.BOTH;
+        c.fill = GridBagConstraints.HORIZONTAL;
         for (int i = 0; i < components.size(); i++) {
             panel.add(components.get(i), c);
             if (i % 2 == 0) {
