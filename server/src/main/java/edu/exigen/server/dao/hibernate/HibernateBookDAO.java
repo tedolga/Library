e95ad01a1aad1bca7becbb8258d079e07bc7ab70
Tedikova Olga <tedolga@gmail.com>
2012-09-27 19:02:31 +0400

    JPA DAO unit test initialization

--- server/src/main/java/edu/exigen/server/dao/hibernate/HibernateBookDAO.java
+++ server/src/main/java/edu/exigen/server/dao/hibernate/HibernateBookDAO.java
@@ -1,13 +1,13 @@
 package edu.exigen.server.dao.hibernate;
 
-import edu.exigen.entities.Book;
-import edu.exigen.server.dao.BookDAO;
-import edu.exigen.server.dao.LibraryDAOException;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import java.util.List;
+
+import edu.exigen.entities.Book;
+import edu.exigen.server.dao.BookDAO;
+import edu.exigen.server.dao.LibraryDAOException;
 
 /**
  * @author Tedikova O.
@@ -38,17 +38,41 @@ public class HibernateBookDAO implements BookDAO {
 
     @Override
     public Book readBook(int id) throws LibraryDAOException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Book book = em.find(Book.class, id);
+        tx.commit();
+        em.close();
+        return book;
     }
 
     @Override
     public List<Book> readAll() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        List<Book> allBooks = em.createQuery("select b from Book b order by b.id", Book.class).getResultList();
+        tx.commit();
+        em.close();
+        return allBooks;
     }
 
     @Override
     public boolean updateBook(int id, Book newBook) throws LibraryDAOException {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Book oldBook = em.find(Book.class, id);
+        oldBook.setIsbn(newBook.getIsbn());
+        oldBook.setAuthor(newBook.getAuthor());
+        oldBook.setTitle(newBook.getTitle());
+        oldBook.setTopic(newBook.getTopic());
+        oldBook.setYear(newBook.getYear());
+        oldBook.setCount(newBook.getCount());
+        tx.commit();
+        em.close();
+        return true;
     }
 
     @Override
--- server/src/test/java/edu/exigen/server/dao/hibernate/HibernateBookDAOTest.java
+++ server/src/test/java/edu/exigen/server/dao/hibernate/HibernateBookDAOTest.java
@@ -3,21 +3,28 @@ package edu.exigen.server.dao.hibernate;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.exigen.entities.Book;
-import edu.exigen.server.dao.BookDAO;
+import junit.framework.Assert;
 
 /**
  * @author O. Tedikova
  * @version 1.0
  */
 public class HibernateBookDAOTest {
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("postgresUnit");
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("postgresUnitWork");
+    private static HibernateBookDAO bookDAO = new HibernateBookDAO();
+    private static int rowCounter = 0;
+
+    @BeforeClass
+    public static void beforeAll() {
+        bookDAO.setEntityManagerFactory(entityManagerFactory);
+    }
 
     @Test
     public void testCreateBook() throws Exception {
-        BookDAO bookDAO = new HibernateBookDAO();
         Book book = new Book();
         book.setAuthor("Pushkin");
         book.setIsbn("ISBN-67676-09-98");
@@ -26,18 +33,34 @@ public class HibernateBookDAOTest {
         book.setYear(1980);
         book.setCount(4);
         int bookId = bookDAO.createBook(book);
+        rowCounter += 1;
+        Assert.assertEquals(book.getIsbn(), bookDAO.readBook(bookId).getIsbn());
 
     }
 
     @Test
     public void testReadBook() throws Exception {
-
-
+        Book book = new Book();
+        book.setAuthor("Gogol");
+        book.setIsbn("ISBN-67676-09-80");
+        book.setTitle("Revizor");
+        book.setTopic("Classic");
+        book.setYear(1970);
+        book.setCount(1);
+        int bookId = bookDAO.createBook(book);
+        rowCounter += 1;
+        Assert.assertEquals(book.getIsbn(), bookDAO.readBook(bookId).getIsbn());
+        Assert.assertEquals(book.getAuthor(), bookDAO.readBook(bookId).getAuthor());
+        Assert.assertEquals(book.getTitle(), bookDAO.readBook(bookId).getTitle());
+        Assert.assertEquals(book.getTopic(), bookDAO.readBook(bookId).getTopic());
+        Assert.assertEquals(book.getYear(), bookDAO.readBook(bookId).getYear());
+        Assert.assertEquals(book.getCount(), bookDAO.readBook(bookId).getCount());
     }
+
 
     @Test
     public void testReadAll() throws Exception {
-
+        Assert.assertEquals(rowCounter, bookDAO.readAll().size());
     }
 
     @Test
